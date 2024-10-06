382b7620841455ad9e3cf14e246f868bded81ead
Ashraful Islam <33465611+hiashraful@users.noreply.github.com>
2024-10-06 19:23:06 +0600

    Pagination Bug Script

--- snippets.js
+++ snippets.js
@@ -1,1 +1,16 @@
+// script for pagination bug in elementor
+// if the pagination does not work 
+// or refresh the page on clicking next
 
+<script>
+jQuery(document).ready(function(){ 
+    jQuery('a.page-numbers').each(function(index) { 
+        var oldUrl = jQuery(this).attr('href'); 
+        var pattern = /\/[0-9]\//;
+        var matchedResult = oldUrl.match(pattern);
+        var newResult = '/page' + matchedResult;
+        var newUrl = oldUrl.replace(matchedResult, newResult);
+        jQuery(this).attr('href', newUrl);
+    }); 
+});
+</script>
